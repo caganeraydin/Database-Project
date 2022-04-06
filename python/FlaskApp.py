9d974549d6c0617424ea2489d159b14dd91c717a
Doga Uras <duras065@uottawa.ca>
2022-04-06 17:18:30 -0400

    Update and Delete are done for Treatment. For Insert, I still need to add the checks to confirm that the dentist_id and chart_no entered exist in the database.

--- python/FlaskApp.py
+++ python/FlaskApp.py
@@ -4,11 +4,12 @@ import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from psycopg2 import Error
+from sqlalchemy import false
 
 from python.deletions import delete_user,deleteTreatment
 from python.getters import get_all_users, get_last_address_id, get_all_addresses, get_user_with_id, get_user_with_email, \
     get_patient, get_employee, get_responsible_party, get_admin, get_insurance_claim, get_payment, get_invoice, \
-    get_hygienist, get_receptionist, get_dentist, get_treatment, get_all_treatments
+    get_hygienist, get_receptionist, get_dentist, get_treatment, get_all_treatments, get_patient_chart
 from python.inserters import insert_user, insert_appointment_procedure, insert_address, insert_user_address_latest, \
     insert_patient, insert_patient_chart, insert_branch, insert_branch_address, insert_invoice, insert_payment, \
     insert_insurance_claim, insert_appointment, insert_fee_charge, insert_receptionist, insert_dentist, \
@@ -33,7 +34,6 @@ def insert_treatment():
 
     # if request.method == 'POST':
 
-    treatment_id = request.form['treatment_id']
     dentist_id = request.form['dentist_id']
     chart_no = request.form['chart_no']
     appointment_type = request.form['appointment_type']
@@ -43,7 +43,6 @@ def insert_treatment():
     tooth = request.form['tooth']
     comments = request.form['comments']
 
-    print(treatment_id)
     print(dentist_id)
     print(chart_no)
     print(appointment_type)
@@ -53,27 +52,28 @@ def insert_treatment():
     print(tooth)
     print(comments)
 
-    # treatment = Treatment(treatment_id, dentist_id, chart_no, appointment_type, treatment_type, medication, symptoms, tooth, comments)
-    #
-    #
-    # db.session.add(treatment)
-    # db.session.commit()
-    insertTreatment(dentist_id, chart_no, appointment_type, treatment_type, medication, symptoms, tooth, comments)
-
-    flash("Treatment Inserted Successfully")
-
-    return redirect(url_for('get_treatments'))
+    if(not (get_patient_chart(chart_no))):
+        print("here1")
+        flash("Chart No you entered does not exist, please try again.")
+        return redirect(url_for('get_treatments'))
+    if(not (get_dentist(dentist_id))):
+        print("here2")
+        flash("Dentist Id you entered does not exist, please try again.")
+        return redirect(url_for('get_treatments'))
+    else:
+        print("here3")
+        insertTreatment(dentist_id, chart_no, appointment_type, treatment_type, medication, symptoms, tooth, comments)
+        flash("Treatment Inserted Successfully")
+        return redirect(url_for('get_treatments'))
 
 
 #this is our update route to modify an already existing treatment
-@app.route('/update_treatment', methods = ['GET', 'POST'])
-def update_treatment():
+@app.route('/update_treatment/<treatment_id>/', methods = ['GET', 'POST'])
+def update_treatment(treatment_id):
 
     if request.method == 'POST':
-        # treatment = Treatment.query.get(treatment_id)
 
-        treatment_id = request.form['treatment_id']
-        dentist_id =request.form['dentist_id']
+        dentist_id = request.form['dentist_id']
         chart_no = request.form['chart_no']
         appointment_type = request.form['appointment_type']
         treatment_type = request.form['treatment_type']
@@ -82,7 +82,6 @@ def update_treatment():
         tooth = request.form['tooth']
         comments = request.form['comments']
 
-        # db.session.commit()
         updateTreatment(treatment_id, dentist_id, chart_no, appointment_type, treatment_type, medication, symptoms, tooth, comments)
         flash("Event Updated Successfully")
 
@@ -92,10 +91,7 @@ def update_treatment():
 #This route is for deleting a treatment
 @app.route('/delete_treatment/<treatment_id>/', methods = ['GET', 'POST'])
 def delete_treatment(treatment_id):
-    # treatment = Treatment.query.get(treatment_id)
-
-    # db.session.delete(treatment)
-    # db.session.commit()
+    print(treatment_id)
     deleteTreatment(treatment_id)
     flash("Treatment Deleted Successfully")
 
